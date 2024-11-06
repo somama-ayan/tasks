4a846444bb57d64c9e94727af2c6f80074bd2855
a-manan123 <manankhokhar1278@gmail.com>
2024-11-06 17:36:25 +0500

    About Section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./About.css";
 
 const About = () => {
-    return (
-        <section id='AboutSection'>
-About
-        </section>
-    )
-}
+  return (
+    <section id="AboutSection">
+      <Container>
+        <Row>
+          <Col sm={8}>
+            <div className="about-card1">
+              <p className="about-head">
+                We are <br />
+                <span>FLeact Tech.</span>
+                <hr className="about-line" />
+              </p>
+              {/* <p className="about-head1">Fleact Tech</p> */}
 
-export default About
+              <p className="about-head-para">
+                Our vision is to pioneer strategic solutions, revolutionizing
+                industries and enriching lives through innovation and
+                excellence. We aim to set new benchmarks in technology,
+                empowering individuals and organizations worldwide for a
+                brighter future.
+              </p>
+              <button>GET A QUOTE TODAY</button>
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default About;

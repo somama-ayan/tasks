2f78c856dbc131bc460a9ad0a2e1c91401667f74
somama Ayan <somama.ayaan@gmail.com>
2024-11-06 17:49:47 +0500

    header home portfolio

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "./About.css";
+import "./Styles/About.css";
 
 const About = () => {
   return (

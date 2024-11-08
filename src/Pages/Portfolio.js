967cc282fb7a1d39a964f7443125ad1bf2c83cbe
somama Ayan <somama.ayaan@gmail.com>
2024-11-08 14:24:34 +0500

    portfolio

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Styles/Portfolio.css";
 import Portfolioblognavbar from "../Components/Portfolioblognavbar";
-import { Col, Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 const Portfolio = () => {
   return (
     <div>
@@ -16,16 +16,34 @@ const Portfolio = () => {
         </div>
       </section>
       <section>
-        <Container>
-          <Col lg={6}>
-            <h1 className="head1Port">
-              Touching the heights of{" "}
-              <span className="head1SpanPort">Creativity</span> and{" "}
-              <span className="head1SpanPort">Success</span>.
-            </h1>
-            <h1><i class="bi bi-arrow-left"></i></h1>
-          </Col>
+        <Container fluid>
+          <Row>
+            <Col lg={7} md={6} sm={12}>
+              <p className="head1Port">
+                Touching the heights of{" "}
+                <span className="head1SpanPort">Creativity</span> and{" "}
+                <span className="head1SpanPort">Success</span>.
+              </p>
+            </Col>
+          </Row>
         </Container>
+        <div className="flex-container">
+          <Col className="PortParaCol">
+            <div>
+              <p className="portfolioPara">
+                <span className="head1SpanPort">50 + </span>
+              </p>
+            </div>
+            
+            <div className="col-lg-6 justify-content-end">
+              <p className="portfolioPara">
+                Projects Successfully crafted and delivered, within a span of{" "}
+                <span className="head1SpanPort">1.5 Years</span>.
+              </p>
+            </div>
+         
+          </Col>
+        </div>
       </section>
     </div>
   );

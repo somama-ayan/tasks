3d29ddf4fe04b660f0a0c95ba4f4704b095d8bb0
a-manan123 <manankhokhar1278@gmail.com>
2024-11-15 23:29:01 +0500

    Contact.css

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import './Styles/Contact.css'
+import React from "react";
+import "./Styles/Contact.css";
 const Contact = () => {
-    return (
-        <div className="contact-form container">
-<section id='ContactSection'>
-<h1 className='text-center'>Let’s get in touch!</h1>
-      <div className="form-inputs">
-        <input
-          type="text"
-          required
-          className="input-field"
-          placeholder="Enter your name"
-          name="username"
-          />
-        <input
-          type="email"
-          required
-          className="input-field"
-          placeholder="Enter your email"
-          name="email"
-          />
-      </div>
-      <div className="full-width">
-        <input
-          required
-          className="input-field"
-          placeholder="Tell us about your project."
-          name="project"
-          />
-      </div>
-      <button className="submit-button">
-        SUBMIT <span>→</span>
-      </button>
-          </section>
-        </div>
-    )
-}
+  return (
+    <div className="contact-form container">
+      <section id="ContactSection">
+        <h1 className="text-center">Let’s get in touch!</h1>
 
-export default Contact
+        <div className="form-inputs">
+          <input
+            type="text"
+            required
+            className="input-field"
+            placeholder="Enter your name"
+            name="username"
+          />
+          <input
+            type="email"
+            required
+            className="input-field"
+            placeholder="Enter your email"
+            name="email"
+          />
+        </div>
+        <div className="full-width">
+          <input
+            required
+            className="input-field"
+            placeholder="Tell us about your project."
+            name="project"
+          />
+        </div>
+        <div className="contact-btn">
+          <button className="submit-button">
+            SUBMIT <span>→</span>
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Contact;
